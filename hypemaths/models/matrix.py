d95123fdf0b8c9d3ceec81b4f7f03ed3de2fd750
janaSunrise <warriordefenderz@gmail.com>
2020-12-25 17:32:25 +0530

    Refactor matrix validation code, stuck for multi layers. Add matrix dimensions.

--- hypemaths/models/matrix.py
+++ hypemaths/models/matrix.py
@@ -4,15 +4,22 @@ from hypemaths.exceptions import InvalidMatrixError
 
 
 class Matrix:
-    def __init__(self, matrix: t.Union[int, float, list] = None, dims: tuple = None, fill: t.Union[int, float] = None) -> None:
+    def __init__(
+            self, matrix: t.Union[int, float, list] = None, dims: tuple = None, fill: t.Union[int, float] = None
+    ) -> None:
         if not matrix:
             if not dims or fill:
                 raise ValueError("You need to pass the dimensions of the matrix or the fill value!")
 
         else:
             self.matrix = self._cleaned_matrix(matrix)
+            self.dims = tuple(self._get_mat_dimension(matrix))
 
-    def _cleaned_matrix(self, matrix: list) -> list:
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.matrix})"
+
+    @staticmethod
+    def _cleaned_matrix(matrix: list) -> list:
         """Checks if a matrix passed is valid or not and returns the clean matrix."""
         if isinstance(matrix, (int, float)):
             return [[matrix]]
@@ -26,11 +33,16 @@ class Matrix:
             if not isinstance(matrix[0], list):
                 return matrix
             else:
-                len_set = set([len(x) for x in matrix])
-                if len(len_set) > 1:
+                is_valid_matrix = all(isinstance(element, list) for element in matrix)
+                if not is_valid_matrix:
                     raise InvalidMatrixError(
                         "Matrix sizes are invalid! Must have same number of element in each sub list."
                     )
                 return matrix
 
         raise InvalidMatrixError("Matrix sizes are invalid!")
+
+    def _get_mat_dimension(self, matrix: list) -> list:
+        if not isinstance(matrix, list):
+            return []
+        return [len(matrix)] + self._get_mat_dimension(matrix[0])
