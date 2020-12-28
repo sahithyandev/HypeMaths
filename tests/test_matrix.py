4ab5029c20cb386286f30947c37ab38dd9aafd2c
Sahithyan <sahithyan2701@gmail.com>
2020-12-28 08:50:28 +0530

    added more tests

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -6,6 +6,7 @@ from hypemaths.exceptions import InvalidMatrixError
 
 class ValidMatrixTests(unittest.TestCase):
     """Tests for checking the validation when a Matrix is initialized."""
+
     def test_valid_matrix(self) -> None:
         test_cases = (
             (Matrix(1), [[1]]),
@@ -30,6 +31,7 @@ class ValidMatrixTests(unittest.TestCase):
 
 class MatrixAttributesTests(unittest.TestCase):
     """Tests for checking the attributes when a Matrix is created."""
+
     def test_matrix_rows(self) -> None:
         test_cases = (
             (Matrix(1), 1),
@@ -56,3 +58,46 @@ class MatrixAttributesTests(unittest.TestCase):
 
         for matrix, column_value in test_cases:
             self.assertEqual(matrix.cols, column_value)
+
+    def test_matrix_dimensions(self) -> None:
+        test_cases = (
+            (Matrix(1), (1, 1)),
+            (Matrix([1, 2, 3, 4]), (1, 4)),
+            (Matrix([[1, 2], [3, 4]]), (2, 2)),
+            (
+                Matrix([
+                    [1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]
+                ]), (3, 3)
+            )
+        )
+
+        for matrix, dimension_value in test_cases:
+            self.assertEqual(matrix.dims, dimension_value)
+
+
+class MatrixTranspositionTests(unittest.TestCase):
+    "Tests for matrix transposition"
+
+    def test_matrix_transposition(self) -> None:
+        test_cases = (
+            (Matrix(1), Matrix(1)),
+            (Matrix([1, 2, 3, 4]), Matrix([
+                [1],
+                [2],
+                [3],
+                [4]
+            ])),
+            (Matrix([
+                [1, 2, 3],
+                [4, 5, 6]
+            ]), Matrix([
+                [1, 4],
+                [2, 5],
+                [3, 6]
+            ]))
+        )
+
+        for matrix, output_matrix in test_cases:
+            self.assertEqual(matrix, output_matrix.transpose())
